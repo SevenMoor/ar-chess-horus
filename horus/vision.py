5a6a9f29728d07bf27bb1da7f0b25b1185fa7913
SevenMoor <au.ottaviano@yahoo.com>
2020-06-10 21:46:26 +0200

    Changed representation of marker from detected to approximated

--- horus/vision.py
+++ horus/vision.py
@@ -42,6 +42,8 @@ class ColorMaskBuilder(object):
 
         x = None
         y = None
+        p1 = None
+        p2 = None
 
         for contour in contours:
             area = cv2.contourArea(contour)
@@ -49,9 +51,12 @@ class ColorMaskBuilder(object):
 
             #TODO Add param to control
             if area > 100 and len(approx)==4:
-                cv2.drawContours(contour_map,[contour],0,(255,0,0),1)
-                moments = cv2.moments(contour)
+                cv2.drawContours(contour_map,[approx],0,(255,0,0),1)
+                moments = cv2.moments(approx)
                 x = int(moments["m10"]/moments["m00"])
                 y = int(moments["m01"]/moments["m00"])
+                p1 = approx[0]
+                p2 = approx[1]
+
 
         return mask,contour_map,(x,y)
